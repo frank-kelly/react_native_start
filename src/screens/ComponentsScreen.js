5dcbe4bbc2ba8f2be3c96ac21a18ab98e6b18806
Frank Kelly <fpskelly@sky.com>
2022-07-04 22:37:38 +0100

    played with views

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-    const myName='Frank';
+    var myName='Frank';
     return <View>
         <Text style={styles.textStyle}>
         Getting Started with React Native
